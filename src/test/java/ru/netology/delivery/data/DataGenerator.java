d79792d60158dd732d2b0fb1c60f3ab50bc49903
Konstantin Sergeev <sergeeff.kossta@gmail.com>
2023-08-11 10:50:38 +0300

    new commit

---
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -0,0 +1,57 @@
+package ru.netology.delivery.data;
+
+import com.github.javafaker.Faker;
+import lombok.Value;
+
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Random;
+
+public class DataGenerator {
+    private DataGenerator() {
+    }
+
+    public static String generateDate(int shift) {
+        return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    public static String generateCity(String locale) {
+        var sities = new String[]{"Москва","Санкт-Петербург","Новосибирск","Екатеринбург","Казань",
+                "Нижний Новгород","Саратов","Красноярск","Самара","Астрахань","Ростов-на-Дону","Ставрополь","Краснодар",
+                "Воронеж","Волгоград","Пермь"};
+        return sities[new Random().nextInt(sities.length)];
+    }
+
+    public static String generateName(String locale) {
+        // TODO: добавить логику для объявления переменной name и задания её значения, для генерации можно
+        // использовать Faker
+        Faker faker = new Faker(new Locale(locale));
+        return faker.name().fullName();
+    }
+
+    public static String generatePhone(String locale) {
+        // TODO: добавить логику для объявления переменной phone и задания её значения, для генерации можно
+        Faker faker = new Faker(new Locale(locale));
+        return faker.phoneNumber().phoneNumber();
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static UserInfo generateUser(String locale) {
+            // TODO: добавить логику для создания пользователя user с использованием методов generateCity(locale),
+            // generateName(locale), generatePhone(locale)
+            return new UserInfo(generateCity(locale),generateName(locale),generatePhone(locale));
+        }
+    }
+
+    @Value
+    public static class UserInfo {
+        String city;
+        String name;
+        String phone;
+    }
+}
